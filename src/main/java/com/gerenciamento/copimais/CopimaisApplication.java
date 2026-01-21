098a0d05b64c67cb6b5099a7c5263f077a9ea7e6
Xaiane Borges <xaianebezerra@gmail.com>
2026-01-21 00:11:54 -0300

    Configurações Iniciais

---
+++ src/main/java/com/gerenciamento/copimais/CopimaisApplication.java
@@ -0,0 +1,13 @@
+package com.gerenciamento.copimais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CopimaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CopimaisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gerenciamento/copimais/CopimaisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gerenciamento.copimais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CopimaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
